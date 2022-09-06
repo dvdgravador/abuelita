4223227a64472bec6fc09095d92ebb8275a3a0a6
VSeryi <VSeryi@users.noreply.github.com>
2022-09-07 00:21:38 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,103 +1,138 @@
-import tkinter as tk
-import os 
+# import external libraries
+from time import sleep
 import vlc
-import time
+import sys
+
+import tkinter as Tk
+from tkinter import PhotoImage, ttk
+from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 
+# import standard libraries
+import os
 
+class Player(Tk.Frame):
+    """The main window has to deal with events.
+    """
+    def __init__(self, parent):
+        Tk.Frame.__init__(self, parent)
 
-directorio = 'pictures'
-cont = os.listdir(directorio)
-contenido= sorted(cont, key=lambda s: int(s.split('.')[0][0:])) # para ordenar los elementos de la lista
+        self.parent = parent
+        self.position = 0
+        self.items = len(os.listdir("pictures"))
+        self.images = {}
+        self.is_playing = False
 
+        self.width_screen = self.parent.winfo_screenwidth()
+        self.height_screen = self.parent.winfo_screenheight()
 
-images = []
-
-for fichero in contenido:                
-    if os.path.isfile(os.path.join(directorio, fichero)) and fichero.endswith('.png'):   # solo si es archivo y con extension jpg lo añade
-        images.append(fichero)
-
-t = len(images)   
-n = 0
-#print(t)
-
-#print(images)
-
-
-
-root = tk.Tk()
-root.attributes('-fullscreen', 1) #fullscreen
-root.config(cursor="none")  # escondo cursor
-
-# obtiene el tamaño actual de la pantalla:
-scr_width = root.winfo_screenwidth()
-scr_height = root.winfo_screenheight()
-
-
-# reescalo las imagenes para llenar pantalla completa
-photos = []
-for x in images:
-    img = Image.open("pictures/" + str(x)) 
-    img = img.resize((int(scr_width), int(scr_height)))  # redimensiona la foto al tamaño de la pantalla 
-    photos.append(ImageTk.PhotoImage(img))     # en cada iteracion se añade una imagen ya redimensionada a la lista "photos"
-    #print("escalando imagen"+ str(x))  
-
-   
-displayCanvas = tk.Label(root)
-displayCanvas.pack(expand=1, fill=tk.BOTH)
- 
-
-def actualizarImagen(imagen):
-    displayCanvas.config(image=photos[imagen])
-
-actualizarImagen(n)  # primera imagen en pantalla
-
-def right(event):
-    global n
-    global t
-
-    if n < t:
-        n += 1
-    if n == t:
-        n = 0
-    actualizarImagen(n)   
+        self.load_images()
     
-def left(event):
-    global n
-    global t
-    n = abs(n)                                 # siempre positivo, nunca negativo ;)
-    if n <= t and n != 0:
-        n -= 1
-    else:
-        n = t-1        
-    print(n)                   
-    actualizarImagen(n)
+        self.videopanel = Tk.Frame(self.parent)
+        self.canvas = Tk.Canvas(self.videopanel)
+
+        self.background_image = self.canvas.create_image(0, 0, image = None, 
+                     anchor = "nw")
+
+        self.update_image()
+
+        self.videopanel.pack(fill=Tk.BOTH,expand=1)
+        self.canvas.pack(fill=Tk.BOTH,expand=1)
+        
+        self.parent.bind('<Escape>', self.OnExit) # permite que se salga del programa pulsando escape
+        self.parent.bind('<Right>', self.OnMove) 
+        self.parent.bind('<Left>', self.OnMove)
+        self.parent.bind('<Return>', self.OnOpen)  
+        self.parent.bind('<<MediaFinish>>', self.OnOpen)  
+        
+        self.Instance = vlc.Instance()
+        self.player = self.Instance.media_player_new()
+        self.events = self.player.event_manager()
+        self.events.event_attach(vlc.EventType.MediaPlayerEndReached, self.mediaFinish)
+
+        self.parent.update()
     
-
-def reproducir(event):
-
+    def load_images(self):
+        for index in range(0, self.items):
+            filename = index + 1
+            img = Image.open("pictures/" + str(filename) + ".png") 
+            img = img.resize((self.width_screen, self.height_screen))
+            self.images[index] = ImageTk.PhotoImage(img)
     
-    media_player = vlc.MediaPlayer()
-    media_player.set_fullscreen(True)
-    media = vlc.Media(str(n+1)+".mp4")
-    media_player.set_media(media)
-    media_player.play()
-    time.sleep(1.5)
-    duration = media_player.get_length() / 1000
-    #print(duration)
-    time.sleep(duration+1.5)
-    media_player.stop()
+    def update_image(self):
+        self.bg = self.images.get(self.position)
+        self.canvas.itemconfig(self.background_image, image = self.bg)
 
+    def OnExit(self, evt):
+        """Closes the window.
+        """
+        self.parent.quit()
+        self.parent.destroy()
+        os._exit(1)
 
+    def OnOpen(self, evt):
+        """Pop up a new dialow window to choose a file, then play the selected file.
+        """
+        sleep(2)
+        self.player.set_hwnd(0)
+        if self.is_playing:
+            if self.player.is_playing():
+                self.player.stop()
+            else:
+                print("VAMOS A REPRODUCRILO")
+                self.Media = self.Instance.media_new("videos/" + str(self.position) + ".mp4")
+                print("VAMOS A 1")
+                self.player.set_media(self.Media)
+                print("VAMOS A 2")
+                self.player.play()
+                print("VAMOS A 3")
+                self.player.stop()
+                print("VAMOS A 4")
+            self.is_playing = False
+        else:
+            self.Media = self.Instance.media_new("videos/" + str(self.position) + ".mp4")
+            self.player.set_media(self.Media)
+            self.player.set_hwnd(self.GetHandle())
+            self.player.play()
+            self.is_playing = True
     
+    def mediaFinish(self, evnt):
+        self.parent.event_generate("<<MediaFinish>>")
     
+    def OnMove(self, evt):
+        if evt.keysym == "Right":
+            self.position += 1
+        else:
+            self.position -= 1
+        self.position %= self.items
+        self.update_image()
+            
+    def GetHandle(self):
+        return self.videopanel.winfo_id()
+
+    def OnStop(self):
+        """Stop the player.
+        """
+        self.player.stop()
+        # reset the time slider
+        self.timeslider.set(0)
+
+    def errorDialog(self, errormessage):
+        """Display a simple error dialog.
+        """
+        tk.tkMessageBox.showerror(self, 'Error', errormessage)
+
+def Tk_get_root():
+    if not hasattr(Tk_get_root, "root"): #(1)
+        Tk_get_root.root= Tk.Tk()  #initialization call is inside the function
+        Tk_get_root.root.attributes('-fullscreen', 1) #fullscreen
+        Tk_get_root.root.config(cursor="none")  # escondo cursor
+    return Tk_get_root.root
 
 
-
-root.bind('<Escape>', lambda e: root.destroy()) # permite que se salga del programa pulsando escape
-root.bind('<Right>', right) 
-root.bind('<Left>', left)
-root.bind('<Return>', reproducir)  
-    
-root.focus_force() # mantiene en primer plano la pantalla
-root.mainloop()
+if __name__ == "__main__":
+    # Create a Tk.App(), which handles the windowing system event loop
+    root = Tk_get_root()
+    player = Player(root)
+    # show the player window centred and run the application
+    root.mainloop()
